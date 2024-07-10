d5a7d7a1823be4acb85f2433897ad6d8a25f59d4
bansi43 <bansiundhad34@gmail.com>
2024-07-10 19:31:57 +0530

    10-7-224

---
+++ javascript/Lecture13/date.js
@@ -0,0 +1,43 @@
+/* jvascript date mathod  */
+
+// let  date=new Date();
+// let  date=new  Date("2019-12-08 10:20");
+// let  date=new  Date(2020,15,20,22,55,11,102);
+// let  date=new  Date(1555512777);
+
+
+// let  date=new Date();
+// console.log(date.toString());
+// console.log(date.toLocaleDateString);
+// console.log(date.toUTCString());
+// console.log(date.toISOString());
+// console.log(date.toDateString());
+// console.log(date.toTimeString());
+
+
+// get $ set
+
+// let  date=new Date();
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getMilliseconds());
+// console.log(date.getTime());
+
+// let  date=new Date();
+// console.log(date);
+// console.log(date.setFullYear(2033));
+// console.log(date.setMonth());
+// console.log(date.setDate());
+// console.log(date.setDay());
+// console.log(date.setHours());
+// console.log(date.setMinutes());
+// console.log(date.setSeconds());
+// console.log(date.setMilliseconds());
+// console.log(date.setTime());
+
